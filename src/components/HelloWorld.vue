aa3fd0c502d1ac13b23c9212c299ec5702a08a7a
Paradox1024 <117451715+Paradox1024@users.noreply.github.com>
2022-11-07 10:33:49 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const alert = ref(false)
+
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,4 @@
+import 'vuetify/styles/main.css'
+import { createVuetify } from 'vuetify'
+
+export default createVuetify()
